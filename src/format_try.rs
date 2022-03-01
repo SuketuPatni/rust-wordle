a707903b2057770b88ac9510f4a5a71560628ad3
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-01 23:38:41 +0530

    removed ansi, put crossterm, fixed game logic

--- src/format_try.rs
+++ src/format_try.rs
@@ -1,20 +1,49 @@
-use ansi_term::Colour::{Green, Yellow, Red};
-use std::io;
-use std::io::Write;
+// use ansi_term::Colour::{Green, Yellow, Red};
+// use std::io;
+use std::io::stdout;
+use crossterm::{
+    execute,
+    style::{Color, Print, SetBackgroundColor, SetForegroundColor, ResetColor, Attribute, SetAttribute}
+};
 
 pub fn format_tried_word(trial: &str, answer: &str) { 
 
     let answer_vec: Vec<char> = answer.chars().collect();
 
     for (i, j) in trial.chars().enumerate() {
+        let formatted_char = format!(" {} \t", j);
         if answer_vec[i] == j {
-            print!("{}\t", Green.paint(j.to_string()));
+            // print!("{}\t", Green.paint(j.to_string()));
+            execute!(
+                stdout(),
+                SetBackgroundColor(Color::DarkGreen),
+                SetForegroundColor(Color::White),
+                SetAttribute(Attribute::Bold),
+                Print(formatted_char),
+                ResetColor
+            );
         } else if answer_vec.contains(&j) {
-            print!("{}\t", Yellow.paint(j.to_string()));
+            // print!("{}\t", Yellow.paint(j.to_string()));
+            execute!(
+                stdout(),
+                SetBackgroundColor(Color::DarkYellow),
+                SetForegroundColor(Color::White),
+                SetAttribute(Attribute::Bold),
+                Print(formatted_char),
+                ResetColor
+            );
         } else {
-            print!("{}\t", Red.paint(j.to_string()));
+            // print!("{}\t", Red.paint(j.to_string()));
+            execute!(
+                stdout(),
+                SetBackgroundColor(Color::DarkRed),
+                SetForegroundColor(Color::White),
+                SetAttribute(Attribute::Bold),
+                Print(formatted_char),
+                ResetColor
+            );
         }
-        io::stdout().flush().unwrap();
+        // io::stdout().flush().unwrap();
     }
 
     println!("");
--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,9 @@ fn main() {
         answers[i] = j;
     }
 
-    let chosen_index = thread_rng().gen_range(0..2313);
-    let chosen_word = answers[chosen_index];
+    // let chosen_index = thread_rng().gen_range(0..2313);
+    // let chosen_word = answers[chosen_index];
+    let chosen_word = "gross";
 
     println!("Welcome to Wordle!");
 
@@ -36,20 +37,27 @@ fn main() {
 
     loop {
 
-        if num_tries == 6 || trial == chosen_word {
-            println!("Your 6 tries are over! The correct word was {}", chosen_word);
-            break;
-        }
-
         if tries.contains(&(trial.as_str())) == false {
             println!("ERROR: Not a valid try!");
         } else {
             format_try::format_tried_word(&trial.as_str(), chosen_word);
+            
+            if num_tries == 6 {
+                println!("Your 6 tries are over!");
+                // The correct word was {}", chosen_word
+                if trial == chosen_word {
+                    println!("Congratulations! You won in {} tries", num_tries + 1);
+                    break;
+                } else {
+                    println!("You lost. Try again!");
+                }
+                break;
+            } 
+
+            num_tries += 1;
         }
 
         trial = input_word();
-
-        num_tries += 1;
     }
 
     
