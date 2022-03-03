b81ed5a8db4db87259a9f9ef8bfdd6ff9ef23164
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-03 15:33:20 +0530

    rust install in script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev
+apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cd ttyd_tools/build
 cmake ..
 make && sudo make install
