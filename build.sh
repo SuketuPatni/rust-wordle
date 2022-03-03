b77581c488632117c8dd5eb924820e1c0c5a19ea
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-03 13:40:34 +0530

    commencing gatsby cloud trial

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev
+apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev
 cd ttyd_tools/build
 cmake ..
 make && sudo make install
 cd ..
 cd ..
-ttyd $(cat .ttyd_env)
