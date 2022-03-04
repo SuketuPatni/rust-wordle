f629d515cba3f1f05959e49872bbb4abef7ce90d
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 14:40:55 +0530

    experimental heroku-community/apt

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 chmod +x -- "$0"
-apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 source $HOME/.cargo/env
 cd ttyd_tools/build
