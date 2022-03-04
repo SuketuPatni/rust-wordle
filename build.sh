2cd306c482c11b6e80878835fdeb2cc0d4675fc3
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 15:05:17 +0530

    removed cmake from build script

--- build.sh
+++ build.sh
@@ -2,17 +2,5 @@
 
 chmod +x -- "$0"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
-git clone https://github.com/tsl0922/ttyd.git ttyd_tools
-cd ttyd_tools
-rm -r .git
-rm -r .github
-rm .gitattributes
-rm .gitignore
-rm LICENSE
-mkdir build && cd  build
-cmake ..
-make && make install
-cd ..
-cd ..
 source $HOME/.cargo/env
 ttyd -t cursorStyle=bar -t lineHeight=1.5 -t disableLeaveAlert=true -t disableResizeOverlay=true-t disableReconnect=true-t titleFixed=rust-wordle -t fontSize=28 bash start.sh
---
+++ ttyd_tools/build/CMakeFiles/ttyd.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ttyd.dir/src/http.c.o"
+  "CMakeFiles/ttyd.dir/src/protocol.c.o"
+  "CMakeFiles/ttyd.dir/src/pty.c.o"
+  "CMakeFiles/ttyd.dir/src/server.c.o"
+  "CMakeFiles/ttyd.dir/src/utils.c.o"
+  "ttyd"
+  "ttyd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ttyd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
