f6e023d47abb2a292de56722ad8b6b1157fbb217
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 15:07:55 +0530

    path to ttyd

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 chmod +x -- "$0"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 source $HOME/.cargo/env
-ttyd -t cursorStyle=bar -t lineHeight=1.5 -t disableLeaveAlert=true -t disableResizeOverlay=true-t disableReconnect=true-t titleFixed=rust-wordle -t fontSize=28 bash start.sh
+./ttyd_tools/build/ttyd -t cursorStyle=bar -t lineHeight=1.5 -t disableLeaveAlert=true -t disableResizeOverlay=true-t disableReconnect=true-t titleFixed=rust-wordle -t fontSize=28 bash start.sh
