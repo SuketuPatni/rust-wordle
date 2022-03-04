ae86d4d37d3b950473789ea597d9c1b060d5ff72
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 15:25:48 +0530

    sensible-browser

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ chmod +x -- "$0"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 source $HOME/.cargo/env
 ./ttyd_tools/build/ttyd -t cursorStyle=bar -t lineHeight=1.5 -t disableLeaveAlert=true -t disableResizeOverlay=true-t disableReconnect=true-t titleFixed=rust-wordle -t fontSize=28 bash start.sh
+sensible-browser localhost:7681
