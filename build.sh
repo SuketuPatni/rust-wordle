22c05f231f627932e65aeaa20cef04020c878dcf
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 14:05:55 +0530

    minor changes

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
 cd ttyd_tools/build
 cmake ..
-make && sudo make install
+make && make install
 cd ..
 cd ..
-ttyd $(cat .ttyd_env)
