97f9aca8667ad8602d29bc3be15c16cd3ae5e824
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 14:49:17 +0530

    build v41

--- build.sh
+++ build.sh
@@ -2,10 +2,17 @@
 
 chmod +x -- "$0"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
-source $HOME/.cargo/env
-cd ttyd_tools/build
+git clone https://github.com/tsl0922/ttyd.git ttyd_tools
+cd ttyd_tools
+rm -r .git
+rm -r .github
+rm .gitattributes
+rm .gitignore
+rm LICENSE
+mkdir build && cd  build
 cmake ..
 make && make install
 cd ..
 cd ..
-ttyd $(cat .ttyd_env)
+source $HOME/.cargo/env
+ttyd -t cursorStyle=bar -t lineHeight=1.5 -t disableLeaveAlert=true -t disableResizeOverlay=true-t disableReconnect=true-t titleFixed=rust-wordle -t fontSize=28 bash start.sh
