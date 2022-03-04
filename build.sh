307ca4b881b976625936ebdcbb8502b6ca66c9b3
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 14:31:13 +0530

    build v40

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 chmod +x -- "$0"
-sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
+apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+source $HOME/.cargo/env
 cd ttyd_tools/build
 cmake ..
-make && sudo make install
+make && make install
 cd ..
 cd ..
