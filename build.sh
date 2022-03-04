dd636c736fd0aeacad285346c9605ce5b9bd4ab2
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-04 14:27:02 +0530

    gave permissions to buildscript

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+chmod +x -- "$0"
+sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 cd ttyd_tools/build
 cmake ..
-make && make install
+make && sudo make install
 cd ..
 cd ..
